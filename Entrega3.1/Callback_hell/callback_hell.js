74cbb9b91575da12a054f7a829624e043d73d762
Victor <vlopezmoles@gmail.com>
2022-06-27 12:15:53 +0200

    actualitzo callback-hell.js

--- Entrega3.1/Callback_hell/callback_hell.js
+++ Entrega3.1/Callback_hell/callback_hell.js
@@ -16,36 +16,43 @@ const reverseText = str =>
   .join("");
 
 // Read and reverse contents of text files in a directory
-/* readdir(inbox, (error, files) => {
+
+const iterationFiles = (error, files) => {
   if (error) return console.log("Error: Folder inaccessible");
-  files.forEach(file => {
-    readFile(join(inbox, file), "utf8", (error, data) => {
-      if (error) return console.log("Error: File error");
-      writeFile(join(outbox, file), reverseText(data), error => {
-        if (error) return console.log("Error: File could not be saved!");
-        console.log(`${file} was successfully saved in the outbox!`);
-      });
+  files.forEach((file) => readingFiles(file));
+};
+
+const readingFiles = (file) => {
+  const result = (error, data) => {
+    if (error) return console.log("Error: File error");
+    reverseFile(data);
+  };
+
+  const reverseFile = (data) => {
+    writeFile(join(outbox, file), reverseText(data), (error) => {
+      if (error) return console.log("Error: File could not be saved!");
+      console.log(`${file} was successfully saved in the outbox!`);
     });
-  });
-}); */
-
-readdir(inbox, checkFolder);
-
-const checkFolder = (error, files) => {
-  if (error) return console.log("Error: Folder inaccessible");
-  files.forEach(readingFile)
-}
-
-const readingFile = file => {
-  readFile (join(inbox, file), "utf8", writingFile);
+  };
   
-const writingFile = (error, data) => {
-  if (error) return console.log("Error: File error");
-  writeFile(join(outbox, file), reverseText(data), filterError);
-}
+  readFile(join(inbox, file), "utf8", result);
+};
 
-const filterError = error => {
-  if (error) return console.log("Error: File could not be saved!");
-  console.log(`${file} was successfully saved in the outbox!`);
-  }
-}
+readdir(inbox, iterationFiles);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

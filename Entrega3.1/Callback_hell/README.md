aa6605eb9a7e714d8d5436f971e994aafaf26601
Victor <vlopezmoles@gmail.com>
2022-06-28 11:22:52 +0200

    README callback hell init

---
+++ Entrega3.1/Callback_hell/README.md
@@ -0,0 +1,12 @@
+# Entrega 3.1 Callback Hell 🔥
+
+## Instruccions d'ús: 🧙‍♂️
+
+He intentat treure els callbacks i modulat el codi a troços per que sigui més interpretable. 
+
+Per executar el programa em de ficar el següent commandament per consola:
+
+    node callback_hell.js
+
+
+
--- Entrega3.1/Singleton/Joc.js
+++ Entrega3.1/Singleton/Joc.js
@@ -8,12 +8,11 @@ class Joc{
     addJugador(jugador) {
         this.jugador.push(jugador);
     };
-    puntuacioMes(puntuacio){
-        this.puntuacio.push(puntuacio);
-    };
-    puntuacioMenys(puntuacio){
-        this.puntuacio.push(puntuacio);
-    }
+    
+    //TODO afegir punts al jugador
+
+    //TODO treure punts al jugador
+
 };
 
 module.exports = Joc;

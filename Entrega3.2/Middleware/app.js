b2704200dc314bbb8cf4491f1a1daf42f832d396
Victor <vlopezmoles@gmail.com>
2022-07-05 22:45:17 +0200

    Update Middleware

--- Entrega3.2/Middleware/app.js
+++ Entrega3.2/Middleware/app.js
@@ -6,4 +6,6 @@ const calculadora = new Calculadora
 
 const input = JSON.parse(fs.readFileSync('./input.json', 'utf8'))
 
-console.log(calculadora.suma(input));
+console.log(`El resultado de la suma és ${calculadora.suma(input)}`);
+console.log(`El resultado de la resta és ${calculadora.resta(input)}`);
+console.log(`El resultado de la multiplicació és ${calculadora.multiplicacio(input)}`);

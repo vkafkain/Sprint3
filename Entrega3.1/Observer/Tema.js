4afb1580f61fa3b3eaa411a8eceac8b095b35efb
Victor <vlopezmoles@gmail.com>
2022-07-05 11:47:55 +0200

    Update Observer

--- Entrega3.1/Observer/Tema.js
+++ Entrega3.1/Observer/Tema.js
@@ -1,23 +1,21 @@
 const EventEmitter = require("events");
-const eventEmitter = new EventEmitter();
 
 class Tema extends EventEmitter {
     constructor(nom){
         super();
         this.nom = nom;
         this.usuaris = [];
-    }
+    };
     suscribe(usuari){
         this.usuaris.push(usuari);
-    }
+    };
 
     missatgeNou(missatge){
-        this.emit(this.nom, missatge);
         this.usuaris.forEach(usuari => {
-            eventEmitter.emit(usuari.nom, missatge);
+            this.emit(usuari.nom, missatge);
             console.log(`${usuari.nom}:te un nou missatge al tema ${this.nom}`)
-        });
-    } 
-}
+        })
+    };
+};
 
 module.exports = Tema;

e7893cd94dcaa5bbf823f130f0f5f4a6f6f24cb5
Victor <vlopezmoles@gmail.com>
2022-07-13 09:10:28 +0200

    Update decorator

---
+++ Entrega3.2/Decorator/decorator.js
@@ -0,0 +1,8 @@
+//TODO arxiu que retorni una funcio(decorator)
+
+//TODO funcio(decorator) efectua una conversio de moneda a euros multiplicant pel coeficient de conversió del fitxet adjunt en funció de la divisa original
+
+//TODO crea aplicació que calculi el cost d'uns quants Articles en euros a partir de les seves divises inicial
+
+//TODO aplicant diferents conversions que usin el Decorator del punt anterior
+
